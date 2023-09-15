8082706310d28410d414d7f49b7c92eec7db5d67
pratikpr <prpr4667@colorado.edu>
2023-09-15 13:57:04 -0700

    [h] 42. Trapping Rain Water - O(n) DP

--- neetcode150/Two Pointers/trapping_rain_water.py
+++ neetcode150/Two Pointers/trapping_rain_water.py
@@ -9,8 +9,27 @@ def trap(height):
     return result
 
 
+def trap_DP(height):
+    result = 0
+    left_max = [0 for _ in range(len(height))]
+    right_max = [0 for _ in range(len(height))]
+
+    left_max[0] = height[0]
+    for i in range(1, len(height)):
+        left_max[i] = max(left_max[i - 1], height[i])
+
+    right_max[len(height) - 1] = height[len(height) - 1]
+    for i in range(len(height) - 2, -1, -1):
+        right_max[i] = max(right_max[i + 1], height[i])
+
+    for i in range(0, len(height)):
+        result += min(left_max[i], right_max[i]) - height[i]
+
+    return result
+
+
 height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
-print(trap(height))
+print(trap_DP(height))
 
 height = [4, 2, 0, 3, 2, 5]
-print(trap(height))
+print(trap_DP(height))
