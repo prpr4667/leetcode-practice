c9a29690f57bf5da293a16a4d26d6f3f6970c5f4
pratikpr <prpr4667@colorado.edu>
2023-09-15 09:25:27 -0700

    [e] 125. Valid Palindrome

---
+++ blind75/Two pointers/valid_palindrome.py
@@ -0,0 +1,21 @@
+def isPalindrome(s: str) -> bool:
+    l, r = 0, len(s) - 1
+
+    while l <= r:
+        while l <= r and not s[l].isalnum():
+            l += 1
+        while l <= r and not s[r].isalnum():
+            r -= 1
+        if s[l].lower() != s[r].lower():
+            return False
+        l += 1
+        r -= 1
+
+    return True
+
+
+s = "A man, a plan, a canal: Panama"
+print(isPalindrome(s))
+
+s = "race a car"
+print(isPalindrome(s))
